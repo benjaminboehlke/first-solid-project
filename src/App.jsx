9360d261f8d1ae23c6b14c520c5bb90d548c7790
Boehlke, Benjamin <boehlke@walter-group.com>
2023-04-13 12:31:24 +0200

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from 'solid-bootstrap'
+
+function App() {  
+  return (
+    <div>
+      <header>
+      <Button variant="danger">Primary</Button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
