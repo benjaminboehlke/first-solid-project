92a16d036d84e1b8841ec5dd8dbae9babedad256
Boehlke, Benjamin <boehlke@walter-group.com>
2023-04-14 12:29:30 +0200

    reset project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { Button } from 'solid-bootstrap'
+import logo from './assets/logo.svg';
+import styles from './App.module.css';
 
-function App() {  
+function App() {
   return (
-    <div>
-      <header>
-      <Button variant="danger">Primary</Button>
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
       </header>
     </div>
   );
